34173ef72de9809fd343591f432e9f4285795b69
Your Name <your_email@example.com>
2025-06-20 12:38:27 +0200

    Vec

--- exercises/vec/vec.cs
+++ exercises/vec/vec.cs
@@ -1,68 +1,65 @@
+using System;
 using static System.Console;
-using static System.Math;
 
-static class main
-{
-    // Extension method to print a double value with an optional label
-    public static void print(this double x, string s = "") { Write(s); WriteLine(x); }
+public class vec{
+    public double x,y,z; //vector components
 
-    // Extension method to print a Vec object with an optional label
-    public static void print(this vec v, string s = "")
-    {
-        Write(s);
-        WriteLine($"{v.x} {v.y} {v.z}");
+    public vec(){
+        x=y=z=0;
     }
 
-    static int Main()
-    {
-        var rnd = new System.Random();
+    public vec(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public static vec operator*(vec v, double c){
+        return new vec(c*v.x,c*v.y,c*v.z);
+    }
 
-        // Generate two random vectors
-        var u = new vec(rnd.NextDouble(), rnd.NextDouble(), rnd.NextDouble());
-        var v = new vec(rnd.NextDouble(), rnd.NextDouble(), rnd.NextDouble());
+    public static vec operator*(double c, vec v){
+        return v*c;
+    }
 
-        // Print the generated vectors
-        u.print("u=");
-        v.print("v=");
+    public static vec operator+(vec v, vec u){
+        return new vec(v.x+u.x, v.y+u.y, v.z+u.z);
+    }
 
-        WriteLine($"u={u}");
-        WriteLine($"v={v}");
-        WriteLine();
+    public static vec operator-(vec u){
+        return new vec(-u.x,-u.y,-u.z);
+    }
 
-        vec t;
+    public static vec operator-(vec v, vec u){
+        return new vec(v.x-u.x, v.y-u.y, v.z-u.z);
+    }
 
-        // Test unary negation (-u)
-        t = new vec(-u.x, -u.y, -u.z);
-        (-u).print("-u =");
-        t.print("t  =");
-        if (vec.Approx(t, -u)) WriteLine("test 'unary -' passed\n");
+    public void print(string s){
+        Write($"{x} {y} {z}");
+    }
 
-        // Test vector subtraction (u - v)
-        t = new vec(u.x - v.x, u.y - v.y, u.z - v.z);
-        (u - v).print("u-v =");
-        t.print("t   =");
-        if (vec.Approx(t, u - v)) WriteLine("test 'operator-' passed\n");
+    public void print(){
+        this.print("");
+    }
 
-        // Test vector addition (u + v)
-        t = new vec(u.x + v.x, u.y + v.y, u.z + v.z);
-        (u + v).print("u+v =");
-        t.print("t   =");
-        if (vec.Approx(t, u + v)) WriteLine("test 'operator+' passed\n");
+    public double dot(vec other) /* to be called as u.dot(v) */
+	{return this.x*other.x+this.y*other.y+this.z*other.z;}
+    public static double dot(vec v,vec w) /* to be called as vec.dot(u,v) */
+	{return v.x*w.x+v.y*w.y+v.z*w.z;}
 
-        // Test scalar multiplication (u * c)
-        double c = rnd.NextDouble();
-        t = new vec(u.x * c, u.y * c, u.z * c);
-        var tmp = u * c; // Workaround for bug in mcs
-        tmp.print("u*c =");
-        t.print("t   =");
-        if (vec.Approx(t, u * c)) WriteLine("test 'operator*' passed\n");
-
-        // Test dot product (u . v)
-        double d = u.x * v.x + u.y * v.y + u.z * v.z;
-        d.print("u%v=");
-        d.print("d  =");
-        if (vec.Approx(d, u.Dot(v))) WriteLine("test 'dot product' passed\n");
-
-        return 0;
+    static bool approx(double a,double b,double acc=1e-9,double eps=1e-9){
+    	if(System.Math.Abs(a-b)<acc)return true;
+	if(System.Math.Abs(a-b)<(System.Math.Abs(a)+System.Math.Abs(b))*eps)return true;
+	return false;
+    }    
+    public bool approx(vec other){
+	if(!approx(this.x,other.x)) return false;
+	if(!approx(this.y,other.y)) return false;
+	if(!approx(this.z,other.z)) return false;
+	return true;
+    }
+    public static bool approx(vec u, vec v) => u.approx(v);
+    
+    public override string ToString(){
+    	return $"{x}, {y}, {z}";
     }
 }
